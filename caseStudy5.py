bd7354f5b829fab9142125146fa32fbff058f6c6
VikrhamRA <32575924+VikrhamRA@users.noreply.github.com>
2018-10-10 09:56:17 +0530

    Update caseStudy5.py

--- caseStudy5.py
+++ caseStudy5.py
@@ -1,8 +1,7 @@
-
-#import numpy as np
+import numpy as np
 import pandas as pd
-#import matplotlib.pyplot as plt
-
+import matplotlib.pyplot as plt
+import statistics as stat
 
 file=open("caseStudy5.txt","r")
 data=[]
@@ -12,15 +11,38 @@ for f in file:
     a=f.strip().split(" ")
     data.append(a)   
 df1=pd.DataFrame(data,columns=['Score1','Score2','Change','Device'])
-df2=df1[df1.columns[0]]
+#df2=df1[df1.columns[0]]
 df3=df1[df1.columns[1]]
 df4=df1[df1.columns[2]]
-score1=df2
-score2=df3
-score3=df4
-B=score3.tolist()
+
+df4=df4.tolist()
 #print(score1)
 #print(score2)
-print(score3)
-print(B)
+print(df4)
+ 
+k=np.arange(1,51)
+del df3[0]  
+
+med1=df3[0:29]
+med2=df3[29:50]
+'''plt.scatter(k, df3, label= "stars", color= "green",  
+        s=10) 
+  
+# x-axis label 
+plt.xlabel('x - axis') 
+# frequency label 
+plt.ylabel('y - axis') 
+# plot title 
+plt.title('My scatter plot!') 
+# showing legend 
+plt.legend() 
+  
+# function to show the plot 
+plt.show()''' 
+
+print(stat.mean(med1))
+print(stat.median(med1))
+
+
+
 
